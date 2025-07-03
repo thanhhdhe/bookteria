c8f67c1f0d907b54b4d2312f16498d83e2090192
Ha Dang Thanh <thanhhdhe176691@fpt.edu.vn>
2025-07-03 19:02:48 +0700

    feat: init api gateway

--- profile-service/src/main/java/com/devteria/profile/controller/InternalUserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/InternalUserProfileController.java
@@ -2,7 +2,6 @@ package com.devteria.profile.controller;
 
 import com.devteria.profile.dto.request.UserProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileCreationResponse;
-import com.devteria.profile.entity.UserProfile;
 import com.devteria.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserProfileController {
+public class InternalUserProfileController {
 
     UserProfileService userProfileService;
 
-    @GetMapping("/users/{profileId}")
-    UserProfileCreationResponse getUserProfile(@PathVariable String profileId) {
-        return userProfileService.getProfile(profileId);
+    @PostMapping("/internal/users")
+    UserProfileCreationResponse createUserProfile(@RequestBody UserProfileCreationRequest request) {
+
+        return userProfileService.createProfile(request);
     }
 }
